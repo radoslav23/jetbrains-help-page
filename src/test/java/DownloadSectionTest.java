6da56325156c72ec81e9089b5c04ecac6a007535
radoslav23 <radoslav_hristov_21@abv.bg>
2026-03-23 11:58:38 +0200

    improvement of method's comments

--- src/test/java/DownloadSectionTest.java
+++ src/test/java/DownloadSectionTest.java
@@ -26,8 +26,6 @@ public class DownloadSectionTest extends BaseTest {
 
     @BeforeMethod
     public void setUpDownloadPage() {
-        //SeleniumDocsPage docsPage = new SeleniumDocsPage(driver);
-        //docsPage.acceptCookies();
         page.acceptCookies();
         page.clickGetIntellijIdeaButton();
     }
